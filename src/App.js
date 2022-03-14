ed280ab7f284989e97be4b96d24c6fc221077cb5
Yuri Zhulev <yuri.zhulyov@gmail.com>
2022-03-14 09:40:37 +0300

    hotfix: load posts in front page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import { useEffect, useState } from 'react';
+
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 import FrontPage from './pages/FrontPage';
 import PostPage from './pages/PostPage';
-import api from './utils/api';
-import { useDispatch } from 'react-redux';
-import { loadingActions } from './store/loadingSlice';
+
 
 function App() {
-  const dispatch = useDispatch();
-
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchItems = async () => {
-      try {
-        dispatch(loadingActions.setLoading(true))
-        const items = await api.getItems();
-        const posts = await Promise.all(items.map(item => {
-          return api.getItem(item)
-        }))
-        setPosts(posts);
-      } catch(err) {
-        console.log(err)
-      } finally {
-        dispatch(loadingActions.setLoading(false))
-      }
-      
-    };
-
-    fetchItems()
-    
-  }, []);
+  
 
   return (
     <Switch>
       <Route path="/:postId">
-        <PostPage posts={posts} />
+        <PostPage/>
       </Route>
       <Route exact path="/">
-        <FrontPage posts={posts} />
+        <FrontPage />
       </Route>
     </Switch>
   );
